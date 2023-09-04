9631668ae9f6330741c14e97f460b458c931a46b
William Aaron <waaron@Williams-Laptop.lan>
2023-09-04 13:53:58 -0400

    init commit

---
+++ terraform/env.sh
@@ -0,0 +1,9 @@
+export app_url=tf-alb-398558612.us-east-1.elb.amazonaws.com
+export aws_region=us-east-1
+export container_name=application
+export ecr_repository_name=app-registry
+export ecr_url=738344517299.dkr.ecr.us-east-1.amazonaws.com/app-registry
+export ecs_cluster=ecs-cluster
+export ecs_service=ecs-service
+export publisher_access_key=AKIA2X2GDH2ZS2V3EOVO
+export publisher_secret_key=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
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,7 @@
+container = {
+  image = "particule/helloworld"
+}
+
+vpc = {
+  id = "vpc-0fefcac498afdd368"
+}
